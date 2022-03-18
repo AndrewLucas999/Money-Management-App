69c891c62ed1fb6e21435e2d967f9dafabaa0078
Andrew Lucas <AndrewLucas999@yahoo.com>
2022-03-18 16:28:07 +0000

    WIP SQL CONNECTION CHECk

---
+++ MoneyManagement/MoneyManagement/Registration.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Data.SqlClient;
+
+namespace MoneyManagement;
+
+public partial class Registration : Window
+{
+    public Registration()
+    {
+        InitializeComponent();
+    }
+
+    private void Submit_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (TextBoxEmail.Text.Length == 0)
+        {
+            TextBoxEmail.Focus();
+        }
+        else if (!Regex.IsMatch(TextBoxEmail.Text,
+                     @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+        {
+            TextBoxEmail.Select(0, TextBoxEmail.Text.Length);
+            TextBoxEmail.Focus();
+        }
+        else
+        {
+            string email = TextBoxEmail.Text;
+            string password = PasswordBox1.Password;
+            SqlConnection con = new SqlConnection();
+            con.Open();
+
+            SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT * FROM Registration WHERE Email ='" + email + "' and Password='" + password + "'", con);
+        }
+    }
+}
