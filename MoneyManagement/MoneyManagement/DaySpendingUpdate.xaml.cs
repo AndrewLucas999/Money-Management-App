651c2addab4752fbf2a3612d7a557a428347c7f7
Andrew Lucas <AndrewLucas999@yahoo.com>
2022-05-16 16:57:17 +0100

    WIP Day spending

--- MoneyManagement/MoneyManagement/DaySpendingUpdate.xaml.cs
+++ MoneyManagement/MoneyManagement/DaySpendingUpdate.xaml.cs
@@ -1,11 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using InternalLibrary.View_Model;
 
 namespace MoneyManagement;
 
 public partial class DaySpendingUpdate : Window
 {
+    public WindowViewModel Model { get; set; }
     public DaySpendingUpdate()
     {
         InitializeComponent();
+    }
+
+    private void SaveAmountSpent_OnClick(object sender, RoutedEventArgs e)
+    {
+        // Model.MathsViewModel.DaySpending = Convert.ToDouble(DayAmountSpent.Text) ;
     }
 }
